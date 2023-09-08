263abc4239b6340fe511918da5e31bc2d5818aa3
Rohit V <rohitedathil@gmail.com>
2023-09-08 21:21:19 +0530

    Configurable llm type

--- ingest.py
+++ ingest.py
@@ -1,12 +1,21 @@
 from repo.mongo import MongoRepository
 from repo.pinecone import PineconeRepository
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
 from dotenv import load_dotenv
+from os import getenv
 
 load_dotenv()
 
 bussiness_repo = MongoRepository()
-vector_repo = PineconeRepository(embedding=OpenAIEmbeddings())
+
+if getenv("LLM") == "openapi":
+    embedding = OpenAIEmbeddings()
+else:
+    embedding = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
+
+vector_repo = PineconeRepository(embedding=embedding)
 
 for product in bussiness_repo.get_products():
     vector_repo.ingest(product)
