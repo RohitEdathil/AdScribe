a2ca0cbd60062d5942412d8b03313c415e4c909a
Rohit V <rohitedathil@gmail.com>
2023-09-08 22:56:50 +0530

    fix

--- repo/vector.py
+++ repo/vector.py
@@ -5,13 +5,10 @@ from langchain.vectorstores.base import VectorStore
 
 
 class VectorRepository(ABC):
+    vectorstore: VectorStore
+
     def __init__(self, embedding: Embeddings) -> None:
         self.embedding = embedding
-
-    @property
-    @abstractmethod
-    def vectorstore(self) -> VectorStore:
-        pass
 
     @abstractmethod
     def ingest(self, product: Product) -> None:
