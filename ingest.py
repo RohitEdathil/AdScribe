027b3bb5e51a7966b43b975faefea82b34497061
Rohit V <rohitedathil@gmail.com>
2023-09-08 21:32:14 +0530

    Added progress bar

--- ingest.py
+++ ingest.py
@@ -3,6 +3,7 @@ from repo.pinecone import PineconeRepository
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
 from dotenv import load_dotenv
 from os import getenv
+from tqdm import tqdm
 
 load_dotenv()
 
@@ -17,5 +18,7 @@ else:
 
 vector_repo = PineconeRepository(embedding=embedding)
 
-for product in bussiness_repo.get_products():
+products = bussiness_repo.get_products()
+for product in tqdm(products, total=len(products), desc="Ingesting products"):
     vector_repo.ingest(product)
+print("Done!")
